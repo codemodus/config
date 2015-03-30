fb09c6fc39ef2835c3ab0184ae0783b88209e10a
Daved <daved@codingmusica.com>
2015-03-30 12:39:07 -0700

    Added comments to satisfy golint.

--- config.go
+++ config.go
@@ -1,3 +1,8 @@
+// Package config provides an interface and init function for handling
+// configuration values stored as JSON.  The JSON structure is defined
+// by a user configurable struct which implements Configurator.  Nested
+// configuration files can be handled so long as InitConfig() is called within
+// the parent InitPost().
 package config
 
 import (
@@ -7,14 +12,17 @@ import (
 )
 
 var (
+	// DefaultConfDir sets the default configuration file parent directory.
 	DefaultConfDir = "/etc/" + path.Base(os.Args[0])
 )
 
+// Configurator defines the basic functionality required to work with config.
 type Configurator interface {
 	ConfDir() string
 	InitPost() error
 }
 
+// InitConfig decodes the provided JSON file into the provided Configurator.
 func InitConfig(c Configurator, filename string) (err error) {
 	dirConf := c.ConfDir()
 	if dirConf == "" {
